a3b0417f68bc6dcff365b3524e69d6771b3a9002
VNNiz <contact@niz.vn>
2023-07-06 01:12:36 +0700

    Change return type for `internalFunction` to Object

--- lib/src/function.dart
+++ lib/src/function.dart
@@ -2,13 +2,12 @@ import 'dart:async';
 
 /// Internal function
 @pragma('vm:entry-point')
-Future<R> internalFunction<R>(dynamic params) async {
-  assert(params is List, 'params is not a list');
-  assert((params as List).length == 2, 'params must have only 2 elements');
-  assert(params[0] is FutureOr<R> Function(dynamic),
-      'params[0] is not a FutureOr<$R> Function(dynamic)');
+Future<Object> internalFunction(List<Object> params) async {
+  // assert(params is List, 'params is not a list');
+  assert(params.length == 2, 'params must have only 2 elements');
+  assert(params[0] is Function, 'params[0] is not a Function');
 
-  Completer<R> completer = Completer();
-  completer.complete(params[0](params[1]));
+  Completer<Object> completer = Completer();
+  completer.complete((params[0] as Function)(params[1]));
   return completer.future;
 }
