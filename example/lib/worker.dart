e99ec77e32008ee09ab3024b71a72cbbc485deb9
lamnhan066 <hello@lamnhan.dev>
2024-04-25 13:50:46 +0700

    Update examples

---
+++ example/lib/worker.dart
@@ -0,0 +1,11 @@
+import 'package:isolates_helper/isolates_helper.dart';
+import 'package:isolates_helper_example/functions.dart';
+
+/// ============================================
+/// dart compile js worker.dart -o worker.js -O4
+/// ============================================
+
+/// In most cases you don't need to modify this function
+main() {
+  IsolatesHelper.workerFunction({'add': add});
+}
