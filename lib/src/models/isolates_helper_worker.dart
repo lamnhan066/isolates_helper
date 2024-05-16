3aeaa44253d068143d363dbe9dc3daaa02401801
lamnhan066 <hello@lamnhan.dev>
2024-05-16 12:19:21 +0700

    Add the generator

---
+++ lib/src/models/isolates_helper_worker.dart
@@ -0,0 +1,8 @@
+/// All functions with this annotation will be combined and generated to the `worker.js`
+///  inside the `web` folder.
+const isolatesHelperWorker = _IsolatesHelperWorker();
+
+/// Worker annotation
+class _IsolatesHelperWorker {
+  const _IsolatesHelperWorker();
+}
